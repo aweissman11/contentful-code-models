d287b0e39a7297cfac60b27f3d01f5868042d469
aweissman11 <weissman.aaron@gmail.com>
2025-07-09 17:01:05 -0400

    Fix CLI version to read from package.json

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 import { syncCommand } from "./sync.js";
 import { migrateCommand } from "./migrate.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pkg = JSON.parse(
+  readFileSync(join(__dirname, "../../package.json"), "utf-8"),
+);
 
 const program = new Command();
 
 program
   .name("ccm")
   .description("CLI tools for managing Contentful content models through code")
-  .version("2.0.1");
+  .version(pkg.version);
 
 program.addCommand(syncCommand);
 program.addCommand(migrateCommand);
